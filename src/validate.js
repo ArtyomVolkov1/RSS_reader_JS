ffd34b3d0219ec270151bcb6d99c734e06259691
Artyom Volkov <artemvolk2099@yandex.ru>
2023-04-26 10:22:48 +0300

    Added validate

---
+++ src/validate.js
@@ -0,0 +1,13 @@
+// import * as yup from 'yup';
+
+// const schema = yup.string().trim().required().url();
+
+// const validate = (url) => {
+//   const isValid = schema
+//     .validate(url)
+//     .then((value) => value)
+//     .catch(() => 'Error');
+//   return console.log(isValid);
+// };
+
+// export default validate;
