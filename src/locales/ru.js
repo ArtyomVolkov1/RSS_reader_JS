292765c480c7cdcd315e0ad8154027ec51c69a09
Artyom Volkov <artemvolk2099@yandex.ru>
2023-05-02 10:37:05 +0300

    Added locales

---
+++ src/locales/ru.js
@@ -0,0 +1,18 @@
+export default {
+  translation: {
+    languages: {
+      ru: 'Русский',
+    },
+    errors: {
+      validation: {
+        notValidUrl: 'Ссылка должна быть валидным URL',
+        alreadyExists: 'RSS уже существует',
+        notRss: 'Ресурс не содержит валидный RSS',
+        fields: 'Это обязательное поле',
+      },
+    },
+    already: {
+      successfully: 'RSS успешно загружен',
+    },
+  },
+};
