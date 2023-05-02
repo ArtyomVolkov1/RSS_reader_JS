480800dbe9e652baabd874ec25d131f20f49954b
Artyom Volkov <artemvolk2099@yandex.ru>
2023-05-02 10:36:47 +0300

    Added change

--- src/view.js
+++ src/view.js
@@ -1,17 +1,46 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-// import onChange from 'on-change';
+import onChange from 'on-change';
 
-const render = (state, elements) => {
-  elements.input.focus();
-  elements.form.reset();
-  const inputEL = elements.input;
-  if (state.form.status === 'invalid') {
-    inputEL.classList.add('is-invalid');
-    inputEL.style.border = '$danger';
-  } else if (state.form.status === 'sending') {
-    inputEL.classList.remove('is-invalid');
-    inputEL.style.border = null;
-  }
+const render = (state, i18n, elements) => {
+  const { input, form, feedback } = elements;
+  input.focus();
+  form.reset();
+  const handleError = () => {
+    if (state.form.error === null) {
+      input.style.border = null;
+    } else {
+      feedback.classList.remove('text-success');
+      feedback.classList.add('text-danger');
+      input.classList.add('is-invalid');
+      input.style.border = '$danger';
+      feedback.textContent = state.form.error;
+    }
+  };
+  const renderAdded = () => {
+    input.classList.remove('is-invalid');
+    feedback.classList.remove('text-danger');
+    feedback.classList.add('text-success');
+    feedback.textContent = i18n.t('already.successfully');
+    input.style.border = null;
+  };
+  const clearErrors = () => {
+    feedback.textContent = '';
+  };
+
+  const watchedState = onChange(state, (path) => {
+    switch (path) {
+      case 'form.error':
+        handleError();
+        break;
+      case 'form.valid':
+        clearErrors();
+        renderAdded();
+        break;
+      default:
+        break;
+    }
+  });
+  return watchedState;
 };
 
 export default render;
